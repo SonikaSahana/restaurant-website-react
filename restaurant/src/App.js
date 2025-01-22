74deac76238166940e71f9be79e916dd32e3f3a7
Sonika Deogharia <deoghariasonika@gmail.com>
2025-01-22 20:20:10 +0530

    restaurant summary

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,11 +1,13 @@
 // App.js
 import React from 'react';
 import Header from './Header';
+import Summary from './Summary';
 
 function App() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   );
 }
