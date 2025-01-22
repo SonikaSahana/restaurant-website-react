432b30e04bb2ec4d4146eb790ee337de40c70b5b
Sonika Deogharia <deoghariasonika@gmail.com>
2025-01-22 19:39:33 +0530

    header

---
+++ restaurant/src/Header.js
@@ -0,0 +1,26 @@
+// Header.js
+import React from 'react';
+import './Header.css';
+import mealsImage from './restaurant.jpg';
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <h1>ReactMeals</h1>
+        <button className="cart-button">
+          <span>Your Cart</span>
+          <span className="cart-count">0</span>
+        </button>
+      </header>
+      <div className="main-image">
+        <img
+          src={mealsImage} 
+          alt="A table full of delicious food"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Header;
